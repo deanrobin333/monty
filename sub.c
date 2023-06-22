ee0b52de3238a9a840a9d1742e44e893de8f8055
deanrobin333 <deanrobin777@gmail.com>
2023-06-22 08:19:03 +0000

    6.sub - sub.c

---
+++ sub.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * sub - Subtracts the second element from the second top element of the stack
+ * @stack: A pointer to the top node of a stack_t linked list.
+ * @line_number: Current working line number of Monty bytecodes file.
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_last_op_tok(swap_failure(line_number, "sub"));
+		return;
+	}
+}
